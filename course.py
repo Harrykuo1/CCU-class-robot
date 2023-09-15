4de6a3609688395e7e5d90fa6a8812004e5e08a8
Harrykuo1 <stu970157@gmail.com>
2023-09-15 12:21:30 +0800

    Reduce the probability of detection

--- course.py
+++ course.py
@@ -1,13 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
-import re
+import logging
+import urllib3
+from fake_useragent import UserAgent
 
 from env import ADD_URL, CLASS_LIST, SEL_LIMIT
 
+def error_log():
+    FORMAT = '%(asctime)s %(levelname)s: %(message)s'
+    logging.basicConfig(level=logging.DEBUG, filename='./bot.log', filemode='a', format=FORMAT)
+    logging.error('Connect error')
+
 def sel_class(session, idx, time):
+    ua = UserAgent()
     headers = {
         "Host": "kiki.ccu.edu.tw",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0",
+        "User-Agent": ua.random,
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "zh-TW,zh;q=0.8,en-US;q=0.5,en;q=0.3",
         "Accept-Encoding": "gzip, deflate, br",
@@ -21,17 +29,25 @@ def sel_class(session, idx, time):
         "Sec-Fetch-Dest": "frame",
         "Sec-Fetch-Mode": "navigate",
         "Sec-Fetch-Site": "same-origin",
-        "Sec-Fetch-User": "?1"
+        "Sec-Fetch-User": "?1",
+        # "Connection": "close",
     }
 
     CLASS_LIST[idx]['session_id'] = session
     data = CLASS_LIST[idx]
-    response = requests.post(ADD_URL, headers = headers, data = data)
+
+    try:
+        urllib3.disable_warnings()
+        response = requests.post(ADD_URL, headers = headers, data = data, verify=False)
+    except:
+        return False
+
     response.encoding = "utf-8"
+    # print(response.text)
     soup = BeautifulSoup(response.text, 'html.parser')
     font_tag = soup.find("font", color="RED")
     if(font_tag.get_text()[0:3] == "(星期"):
-        print("Class: %d, %s time: %d / %d" % (idx, "T", time, SEL_LIMIT))
+        print("Class: %d, %s time: %d/%d" % (idx, "T", time, SEL_LIMIT))
         return True
     
     print("Class: %d, %s time: %d/%d" % (idx, "F", time, SEL_LIMIT))
