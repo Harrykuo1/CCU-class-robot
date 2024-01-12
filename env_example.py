2a59a8c87b600e7dad933b4af3f46ae53a7b5524
Harrykuo1 <stu970157@gmail.com>
2024-01-13 02:25:03 +0800

    Add env_example.py

---
+++ env_example.py
@@ -0,0 +1,39 @@
+# Account info
+USERID = 
+PASSWORD = ""
+
+# URL info
+CAPTCHA_URL = "https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/class_new/captcha.php"
+VERIFY_URL = "https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/class_new/verify_captcha.php"
+LOGIN_URL = "https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/class_new/bookmark.php"
+LOGOUT_URL = "https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/class_new/logout.php"
+ADD_URL = "https://kiki.ccu.edu.tw/~ccmisp06/cgi-bin/class_new/Add_Course01.cgi"
+
+SEL_LIMIT = 150
+THREAD_NUM = 1
+
+CLASS_LIST = [
+    # 一4,5 理財與生活
+    {
+        "session_id": "",
+        "dept": "I001",
+        "grade": "1",
+        "cge_cate": "2",
+        "cge_subcate": "5",
+        "page": "2",
+        "e": "0",
+        "m": "0",
+        "SelectTag": "1",
+        "7405013_01": "3",
+        "7405014_01": "3",
+        "7405014_02": "3",
+        "7405019_01": "3",
+        "course": "7406018_01",
+        "7406018_01": "3",
+        "7406018_02": "3",
+        "7406022_01": "3",
+        "7406032_01": "3",
+        "7406032_02": "3",
+        "7406032_03": "3"
+    },
+]
